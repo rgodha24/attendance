d4d22c477def90ffe79484cb77d7f1d843362a94
Rohan Godha <rgodha24@brophybroncos.org>
2023-08-21 09:59:24 -0700

    publish web

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,30 +1,24 @@
-import { StackContext, Api, EventBus } from "sst/constructs";
+import { StackContext, Api, StaticSite } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
-  const bus = new EventBus(stack, "bus", {
-    defaults: {
-      retries: 10,
-    },
-  });
-
   const api = new Api(stack, "api", {
     defaults: {
-      function: {
-        bind: [bus],
-      },
+      function: {},
     },
-    routes: {
-      "GET /": "packages/functions/src/lambda.handler",
-      "GET /todo": "packages/functions/src/todo.list",
-      "POST /todo": "packages/functions/src/todo.create",
-    },
+    routes: {},
   });
 
-  bus.subscribe("todo.created", {
-    handler: "packages/functions/src/events/todo-created.handler",
+  const site = new StaticSite(stack, "site", {
+    path: "packages/web",
+    buildOutput: "dist",
+    buildCommand: "pnpm build",
+    environment: {
+      VITE_API_URL: api.url,
+    },
   });
 
   stack.addOutputs({
     ApiEndpoint: api.url,
+    SiteUrl: site.url,
   });
 }
