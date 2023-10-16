67e7682b3139c17f1225144e72c9bd6248717558
Rohan Godha <rgodha24@brophybroncos.org>
2023-10-16 15:11:52 -0700

    fix build error

--- packages/stats/astro.config.mjs
+++ packages/stats/astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import aws from "astro-sst/lambda";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  mode: "server",
+  adapter: aws(),
 });
