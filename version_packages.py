50564931f3fdcf09b4cb2ad721830247e6f7cbd1
Rohan Godha <rgodha24@brophybroncos.org>
2023-10-18 18:51:16 -0700

    fixed versioning bug

--- version_packages.py
+++ version_packages.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 from gitignore_parser import parse_gitignore
 
 # Parse .gitignore
@@ -9,15 +10,16 @@ matches = parse_gitignore('.gitignore')
 old_version = sys.argv[1]
 new_version = sys.argv[2]
 
+# Prepare the regular expression pattern
+pattern = re.compile(r'\b' + re.escape(old_version) + r'\b')
+
 # Walk through the directory
 for root, dirs, files in os.walk("."):
     for file in files:
         if (file == "package.json" or file == "Cargo.toml") and not matches(os.path.join(root, file)):
             with open(os.path.join(root, file), 'r+') as f:
                 data = f.read()
-                data = data.replace(old_version, new_version)
+                data = pattern.sub(new_version, data)
                 f.seek(0)
                 f.write(data)
                 f.truncate()
-
-
